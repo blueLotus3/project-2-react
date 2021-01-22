56a80724f1b08a6288e7935cdb5ccdc000274506
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-01-21 20:52:51 -0800

    needs more work, was trying to make a   scrollable horizontal row of cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import Beers from '../pages/Beers.js'
+
 const Home = (props) => {
 return (
-      
-<div className="relax">
-      <section className="section-home">       
-      <h1>Beer Bible</h1>
-      
-      <Beers />
-      </section>
-</div>
-
+      <>
+              <h1>Beer Bible</h1>
+     <section className="section-cards">
+      <div class="cards-wrapper">
+      <div class="card"><p>Beer 1</p></div>
+      <div class="card"><p>Beer 2</p></div>
+      <div class="card"><p>Beer 3</p></div>
+      <div class="card"><p>Beer 4</p></div>
+      <div class="card"><p>Beer 5</p></div>
+      <div class="card"><p>Beer 6</p></div>
+      <div class="card"><p>Beer 7</p></div>
+      <div class="card"><p>Beer 9</p></div>
+      <div class="card"><p>Beer 9</p></div>
+      <div class="card"><p>Beer 10</p></div>
+    </div>
+    </section> 
+</>
 
 )
 
